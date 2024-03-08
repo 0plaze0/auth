cda55f78c063ec85c6eaa3dc8c8cc797fbf125a5
plaze <banikakash131@gmail.com>
2024-03-08 23:11:44 +0530

    feat(utils): valid email and strong password

--- controllers/register.js
+++ controllers/register.js
@@ -1,20 +1,41 @@
 import "dotenv/config.js";
 import User from "../model/userSchema.js";
-import { hashPassword } from "./../utils/authUtils.js";
+import {
+  hashPassword,
+  strongPassword,
+  checkEmail,
+} from "./../utils/authUtils.js";
 
 const registerUser = async (req, res) => {
   const { username, email, password } = req.body;
 
-  //check if password exists
-  //regex for email and password
+  if (!username || !email || !password)
+    return res.status(401).json({ message: "Fill all the field" });
+
+  //regex for valid email
+  if (!checkEmail(email))
+    return res.status(401).json({ message: "send a valid email" });
+
+  //regex for stronger password
+  if (!strongPassword(password))
+    return res.status(401).json({ message: "make a stronger password" });
+
   try {
-    const hashPwd = hashPassword(password, process.env.SALT);
+    //check for duplicate
+    const duplicate = await User.findOne({ email }).exec();
+    if (duplicate)
+      return res.status(401).json({ message: "email already exists" });
+
+    //password hashing
+    const salt = parseInt(process.env.SALT);
+    const hashPwd = await hashPassword(password, salt);
+
     const newUser = {
       username,
       email,
-      password: hashPassword,
+      password: hashPwd,
     };
-    const result = await User.create;
+    const result = await User.create(newUser);
     res.status(200).json(result);
   } catch (err) {
     res.status(500).json({ error: true, message: err.message });
--- utils/authUtils.js
+++ utils/authUtils.js
@@ -6,4 +6,14 @@ const hashPassword = async (password, saltRound) => {
   return hsPwd;
 };
 
-export { hashPassword };
+const strongPassword = (password) => {
+  const regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+  return regex.test(password);
+};
+
+const checkEmail = (email) => {
+  const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return regex.test(email);
+};
+
+export { hashPassword, strongPassword, checkEmail };
